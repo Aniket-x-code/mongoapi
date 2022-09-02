3b63b56d0d07cefead3b813303f7ef4b95c58474
Aniket-x-code <aniket.209301578@muj.manipal.edu>
2022-09-03 00:59:47 +0530

    accesing atlas db and using it ✅

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const { MongoClient } = require('mongodb')
 let dbConnection
 
+let uri= 'mongodb+srv://aniket:aniket123@cluster0.ggb9fc5.mongodb.net/?retryWrites=true&w=majority'
 module.exports={
     connectToDb: (cb)=>{
-        MongoClient.connect('mongodb://localhost:27017/bookstore')
+        MongoClient.connect(uri)
         .then((client)=>{
           dbConnection = client.db()
           return cb()
